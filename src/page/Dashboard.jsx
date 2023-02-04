9dae8f3b2fe15c902cda39aa63a6e41a23c7a494
supermin199x <s.statiion@icloud.com>
2023-02-04 16:54:47 +0700

    4-2-66

--- src/page/Dashboard.jsx
+++ src/page/Dashboard.jsx
@@ -1,36 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import Appbar from '../component/Appbar.jsx';
-import Alert from '@mui/material/Alert';
 import axios from 'axios';
 
 
 export function Dashboard() {
     const [seconds, setSeconds] = useState(0);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-        setSeconds(seconds => seconds + 1);
-        }, 1000);
-        return () => clearInterval(interval);
-    }, []);
-
     const url = 'http://localhost:5000/users';
 
     axios.get(url).then((response) => {
-        
-    const result = response;
-
-    console.log(result);
+        console.log(response);
     })
     .catch((error) => {
-    // handle errors
+        // handle errors
     });
 
     return (
         <>
             <Appbar />
             {seconds}
-            
         </>    
     );
 }
